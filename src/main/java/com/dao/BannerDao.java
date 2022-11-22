e28e4698eed6b176635e91a13cf8787ad0c07687
OpMAY <kevin8622@naver.com>
2022-11-22 19:21:25 +0900

    banner part done

--- src/main/java/com/dao/BannerDao.java
+++ src/main/java/com/dao/BannerDao.java
@@ -1,9 +1,12 @@
 package com.dao;
 
 import com.mapper.BannerMapper;
+import com.model.service.Banner;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Repository
 @Slf4j
@@ -11,5 +14,37 @@ public class BannerDao {
     private final BannerMapper mapper;
     private BannerDao(SqlSession sqlSession) {
         this.mapper = sqlSession.getMapper(BannerMapper.class);
+    }
+
+    public ArrayList<Banner> getBanners(){
+        return mapper.getBanners();
+    }
+
+    public ArrayList<Banner> getActiveBanners(){
+        return mapper.getActiveBanners();
+    }
+
+    public void registerBanner(Banner banner){
+        mapper.registerBanner(banner);
+    }
+
+    public void deleteBanner(int no){
+        mapper.deleteBanner(no);
+    }
+
+    public void updateBannerImage(Banner banner){
+        mapper.updateBannerImage(banner);
+    }
+
+    public void updateBannerUrl(Banner banner){
+        mapper.updateBannerUrl(banner);
+    }
+
+    public void updateBannerStatus(int no){
+        mapper.updateBannerStatus(no);
+    }
+
+    public void updateBanner(Banner banner){
+        mapper.updateBanner(banner);
     }
 }
--- src/main/java/com/mapper/BannerMapper.java
+++ src/main/java/com/mapper/BannerMapper.java
@@ -1,4 +1,23 @@
 package com.mapper;
 
+import com.model.service.Banner;
+
+import java.util.ArrayList;
+
 public interface BannerMapper {
+    ArrayList<Banner> getBanners();
+
+    ArrayList<Banner> getActiveBanners();
+
+    void registerBanner(Banner banner);
+
+    void deleteBanner(int no);
+
+    void updateBannerImage(Banner banner);
+
+    void updateBannerUrl(Banner banner);
+
+    void updateBannerStatus(int no);
+
+    void updateBanner(Banner banner);
 }
