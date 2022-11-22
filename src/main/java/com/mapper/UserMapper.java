a2262b73af7fafeebe58f807203a997cb9d98fc6
OpMAY <kevin8622@naver.com>
2022-11-23 02:03:53 +0900

    user mapper done

--- src/main/java/com/mapper/UserMapper.java
+++ src/main/java/com/mapper/UserMapper.java
@@ -1,4 +1,22 @@
 package com.mapper;
 
+import com.model.User;
+import com.model.UserType;
+
 public interface UserMapper {
+    void registerUser(User user);
+
+    UserType getUserType(int no);
+
+    User loginUser(String id);
+
+    void updateUserLoginDate(int no);
+
+    void updateUserName(User user);
+
+    void updateUserPhone(User user);
+
+    void updateUserProfileImage(User user);
+
+    void deleteUser(int no);
 }
