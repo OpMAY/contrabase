0dc81bac231978128bd2b61159e82355dc4ded8f
OpMAY <kevin8622@naver.com>
2022-11-23 03:01:13 +0900

    supplier mapper done

--- src/main/java/com/dao/SupplierDao.java
+++ src/main/java/com/dao/SupplierDao.java
@@ -1,9 +1,12 @@
 package com.dao;
 
 import com.mapper.SupplierMapper;
+import com.model.service.Supplier;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Repository
 @Slf4j
@@ -11,5 +14,33 @@ public class SupplierDao {
     private final SupplierMapper mapper;
     private SupplierDao(SqlSession sqlSession) {
         this.mapper = sqlSession.getMapper(SupplierMapper.class);
+    }
+
+    public void registerSupplier(Supplier supplier) {
+        mapper.registerSupplier(supplier);
+    }
+
+    public Supplier getSupplierInfoByUserNo(int user_no) {
+        return mapper.getSupplierInfoByUserNo(user_no);
+    }
+
+    public Supplier getSupplierInfoByNo(int no) {
+        return mapper.getSupplierInfoByNo(no);
+    }
+
+    public void applySupplier(int no) {
+        mapper.applySupplier(no);
+    }
+
+    public void updateSupplier(Supplier supplier) {
+        mapper.updateSupplier(supplier);
+    }
+
+    public ArrayList<Supplier> getSuppliers() {
+        return mapper.getSuppliers();
+    }
+
+    public void deleteSupplier(int no) {
+        mapper.deleteSupplier(no);
     }
 }
--- src/main/java/com/mapper/SupplierMapper.java
+++ src/main/java/com/mapper/SupplierMapper.java
@@ -1,4 +1,23 @@
 package com.mapper;
 
+import com.model.service.Supplier;
+
+import java.util.ArrayList;
+
 public interface SupplierMapper {
+    void registerSupplier(Supplier supplier);
+
+    Supplier getSupplierInfoByUserNo(int user_no);
+
+    Supplier getSupplierInfoByNo(int no);
+
+    void applySupplier(int no);
+
+    void updateSupplier(Supplier supplier);
+
+    ArrayList<Supplier> getSuppliers();
+
+    void deleteSupplier(int no);
+
+    boolean checkSupplierApplied(int no);
 }
