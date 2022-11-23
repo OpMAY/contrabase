83a9a30c0a64deb591cf6815c4071c580594c96e
OpMAY <kevin8622@naver.com>
2022-11-23 09:54:18 +0900

    work apply mappers set

--- src/main/java/com/dao/WorkApplyDao.java
+++ src/main/java/com/dao/WorkApplyDao.java
@@ -1,9 +1,15 @@
 package com.dao;
 
 import com.mapper.WorkApplyMapper;
+import com.model.service.work.APPLY_STATUS;
+import com.model.service.work.Work;
+import com.model.service.work.WorkApply;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 
 @Repository
 @Slf4j
@@ -11,5 +17,25 @@ public class WorkApplyDao {
     private final WorkApplyMapper mapper;
     private WorkApplyDao(SqlSession sqlSession) {
         this.mapper = sqlSession.getMapper(WorkApplyMapper.class);
+    }
+
+    public void insertWorkApply(WorkApply workApply) {
+        mapper.insertWorkApply(workApply);
+    }
+
+    public ArrayList<Work> getEmployeeApplyListByStatus(int employee_no, APPLY_STATUS status) {
+        return mapper.getEmployeeApplyListByStatus(employee_no, status);
+    }
+
+    public void updateWorkApplyStatus(int no, APPLY_STATUS status) {
+        mapper.updateWorkApplyStatus(no, status);
+    }
+
+    public boolean checkApplyExists(WorkApply workApply) {
+        return mapper.checkApplyExists(workApply);
+    }
+
+    public void deleteWorkApply(int no) {
+        mapper.deleteWorkApply(no);
     }
 }
--- src/main/java/com/mapper/WorkApplyMapper.java
+++ src/main/java/com/mapper/WorkApplyMapper.java
@@ -1,4 +1,20 @@
 package com.mapper;
 
+import com.model.service.work.APPLY_STATUS;
+import com.model.service.work.Work;
+import com.model.service.work.WorkApply;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.ArrayList;
+
 public interface WorkApplyMapper {
+    void insertWorkApply(WorkApply workApply);
+
+    ArrayList<Work> getEmployeeApplyListByStatus(@Param("employee_no") int employee_no, @Param("status") APPLY_STATUS status);
+
+    void updateWorkApplyStatus(@Param("no") int no, @Param("status") APPLY_STATUS status);
+
+    boolean checkApplyExists(WorkApply workApply);
+
+    void deleteWorkApply(int no);
 }
