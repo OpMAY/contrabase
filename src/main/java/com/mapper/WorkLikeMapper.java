a4f102660ec79106f324e7d49e692d54590e894e
OpMAY <kevin8622@naver.com>
2022-11-23 09:15:28 +0900

    work like mapper done

--- src/main/java/com/mapper/WorkLikeMapper.java
+++ src/main/java/com/mapper/WorkLikeMapper.java
@@ -1,4 +1,17 @@
 package com.mapper;
 
+import com.model.service.work.Work;
+import com.model.service.work.WorkLike;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.ArrayList;
+
 public interface WorkLikeMapper {
+    boolean checkEmployeeWorkLiked(@Param("work_no") int work_no, @Param("employee_no") int employee_no);
+
+    ArrayList<Work> getEmployeeLikedWorks(int employee_no);
+
+    void insertWorkLike(WorkLike workLike);
+
+    void deleteWorkLike(WorkLike workLike);
 }
